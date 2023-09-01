2e131f6d9cb1dbe2845a425f524850e24b0953f6
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-09-01 23:07:42 +0530

    second way 95

--- 95.js
+++ 95.js
@@ -21,3 +21,35 @@ function drawPattern(X) {
   }
   
   console.log(drawPattern(5));
+
+
+  // second way 
+
+  // the distance from one point to another, assuming we can only move
+// left or right and up or down
+const distance = (point_a, point_b) =>
+// distance left and right
+Math.abs(point_a[0] - point_b[0]) +
+  // distance up and down
+  Math.abs(point_a[1] - point_b[1]);
+
+const make_pattern = (side_length) => {
+const halfish_length = Math.floor(side_length / 2);
+const middle = [halfish_length, halfish_length];
+
+return Array.from(
+  { length: side_length },
+  (_, row_i) => Array.from(
+    { length: side_length },
+    (_, col_i) => distance(middle, [col_i, row_i]) > halfish_length ? 'o' : 'x'
+  )
+);
+};
+
+const draw_pattern = (side_length) =>
+make_pattern(side_length)
+  .map((row) => row.join(''))
+  .join("\n");
+
+console.log(draw_pattern(5));
+console.log(draw_pattern(7));
