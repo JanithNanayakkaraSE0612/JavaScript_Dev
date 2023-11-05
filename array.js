ff27e878fa879ce3d9edeebc31da306c9731c4ac
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-06 00:46:24 +0530

    For Loop array learn

--- array.js
+++ array.js
@@ -1,15 +1,27 @@
-let fruits = ["apple","orange","banana"];
+let fruits = ["apple","orange","banana","mango"];
 
 console.log(fruits);
 
 console.log(fruits[0]);
 
-fruits.push("mango");
-fruits.pop();
-fruits.unshift("Coconut");
+//fruits.push("mango");
+//fruits.pop();
+/*fruits.unshift("Coconut");
+fruits.shift();*/
+
+
 
 console.log(fruits[3]);
 
 
 console.log(fruits);
 
+let numOfFruits = fruits.length;
+let index = fruits.indexOf("apple");
+
+console.log(numOfFruits);
+console.log(index);
+
+for(let i = 1 ; i<fruits.length;i+=2){
+    console.log(fruits[i]);
+}
