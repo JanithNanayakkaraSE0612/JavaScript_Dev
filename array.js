a8474b10f7becee8696a5aeddf64ade74a423aca
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-05 20:24:12 +0530

    Array Simplyfy

---
+++ array.js
@@ -0,0 +1,15 @@
+let fruits = ["apple","orange","banana"];
+
+console.log(fruits);
+
+console.log(fruits[0]);
+
+fruits.push("mango");
+fruits.pop();
+fruits.unshift("Coconut");
+
+console.log(fruits[3]);
+
+
+console.log(fruits);
+
